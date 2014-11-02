c5a20531ff0756747c6ee8acb1ac950294f53ec0
tisane <nathanlarson3141@gmail.com>
2014-11-02 16:11:00 -0500

    Initial commit

---
+++ Purge.i18n.php
@@ -0,0 +1,591 @@
+<?php
+/**
+ * Internationalisation file for extension Purge.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Ã†var ArnfjÃ¶rÃ° Bjarmason
+ */
+$messages['en'] = array(
+	'purge' => 'Purge',
+	'purge-desc' => 'Adds a purge tab on all normal pages allowing for quick purging of the cache',
+);
+
+/** Message documentation (Message documentation)
+ * @author EugeneZelenko
+ * @author Purodha
+ * @author Siebrand
+ * @author The Evil IP address
+ */
+$messages['qqq'] = array(
+	'purge' => '{{Identical|Purge}}',
+	'purge-desc' => '{{desc}} "purge" refers to {{msg-mw|purge}}.',
+);
+
+/** Arabic (Ø§Ù„Ø¹Ø±Ø¨ÙŠØ©)
+ * @author DRIHEM
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'purge' => 'ØªØ·Ù‡ÙŠØ±',
+	'purge-desc' => 'ÙŠØ¶ÙŠÙ Ø´Ø±ÙŠØ· ØªØ·Ù‡ÙŠØ± Ø¹Ù„Ù‰ ÙƒÙ„ Ø§Ù„ØµÙØ­Ø§Øª Ø§Ù„Ø¹Ø§Ø¯ÙŠØ© Ù„Ù„Ø³Ù…Ø§Ø­ Ø¨Ø§Ù„ØªØ¸Ù‡ÙŠØ± Ø§Ù„Ø³Ø±ÙŠØ¹ Ù„Ø°Ø§ÙƒØ±Ø© Ø§Ù„ØªØ®Ø²ÙŠÙ†',
+);
+
+/** Egyptian Spoken Arabic (Ù…ØµØ±Ù‰)
+ * @author Meno25
+ */
+$messages['arz'] = array(
+	'purge' => 'Ø¥ÙØ±Ø§Øº Ø§Ù„ÙƒØ§Ø´',
+	'purge-desc' => 'ÙŠØ¶ÙŠÙ Ù„Ø³Ø§Ù† Ø¥ÙØ±Ø§Øº Ù„Ù„ÙƒØ§Ø´ ÙÙ‰ ÙƒÙ„ Ø§Ù„ØµÙØ­Ø§Øª Ø§Ù„Ø¹Ø§Ø¯ÙŠØ© ÙˆÙŠØªÙØ§Ø¯Ù‰ ØªØ­Ù‚Ù‚ Ø§Ù„ÙƒØ§Ø´ Ù„Ù„Ù…Ø³ØªØ®Ø¯Ù…ÙŠÙ† Ø§Ù„Ù…Ø¬Ù‡ÙˆÙ„ÙŠÙ† Ù…Ù…Ø§ ÙŠØ³Ù…Ø­ Ø¨Ø¥ÙØ±Ø§Øº Ø§Ù„ÙƒØ§Ø´ Ø¨Ø´ÙƒÙ„ Ø³Ø±ÙŠØ¹',
+);
+
+/** Asturian (asturianu)
+ * @author Xuacu
+ */
+$messages['ast'] = array(
+	'purge' => 'Purgar',
+	'purge-desc' => 'Amiesta una llingÃ¼eta purgar en toles pÃ¡xines normales que permite la purga rÃ¡pida de la cachÃ©',
+);
+
+/** Azerbaijani (azÉ™rbaycanca)
+ * @author Sortilegus
+ */
+$messages['az'] = array(
+	'purge' => 'tÉ™mizlÉ™',
+	'purge-desc' => 'BÃ¼tÃ¼n adi sÉ™hifÉ™lÉ™rÉ™ tÉ™mizlÉ™mÉ™ niÅŸanÄ±nÄ± É™lavÉ™ edir vÉ™ keÅŸi tez bir zamanda tÉ™mizlÉ™mÉ™yÉ™ imkan verÉ™n anonim istifadÉ™Ã§ilÉ™r Ã¼Ã§Ã¼n tÉ™mizlÉ™mÉ™ yoxlanÄ±ÅŸÄ±nÄ± keÃ§irir',
+);
+
+/** Bavarian (Boarisch)
+ * @author Man77
+ */
+$messages['bar'] = array(
+	'purge' => 'in Cache laarn',
+);
+
+/** Belarusian (TaraÅ¡kievica orthography) (Ð±ÐµÐ»Ð°Ñ€ÑƒÑÐºÐ°Ñ (Ñ‚Ð°Ñ€Ð°ÑˆÐºÐµÐ²Ñ–Ñ†Ð°)â€Ž)
+ * @author Jim-by
+ * @author Wizardist
+ */
+$messages['be-tarask'] = array(
+	'purge' => 'Ð°Ñ‡Ñ‹ÑÑŒÑ†Ñ–Ñ†ÑŒ',
+	'purge-desc' => 'Ð”Ð°Ð´Ð°Ðµ Ð·Ð°ÐºÐ»Ð°Ð´ÐºÑƒ Ð°Ñ‡Ñ‹ÑÑ‚ÐºÑ– Ð½Ð° ÑžÑÐµ Ð·Ð²Ñ‹Ñ‡Ð°Ð¹Ð½Ñ‹Ñ ÑÑ‚Ð°Ñ€Ð¾Ð½ÐºÑ– Ð´Ð»Ñ Ñ…ÑƒÑ‚ÐºÐ°Ð¹ Ð°Ñ‡Ñ‹ÑÑ‚ÐºÑ– ÐºÑÑˆÑƒ',
+);
+
+/** Bulgarian (Ð±ÑŠÐ»Ð³Ð°Ñ€ÑÐºÐ¸)
+ * @author DCLXVI
+ */
+$messages['bg'] = array(
+	'purge' => 'Ð¸Ð·Ñ‡Ð¸ÑÑ‚Ð²Ð°Ð½Ðµ',
+);
+
+/** Breton (brezhoneg)
+ * @author Fulup
+ */
+$messages['br'] = array(
+	'purge' => 'spurjaÃ±',
+	'purge-desc' => 'OuzhpennaÃ± a ra ur pempilh spurjaÃ± war an holl bajennoÃ¹ normal evit gallout spurjaÃ± ar grubuilh fonnus',
+);
+
+/** Bosnian (bosanski)
+ * @author CERminator
+ */
+$messages['bs'] = array(
+	'purge' => 'oÄisti',
+	'purge-desc' => 'Dodaje jeziÄak za ÄiÅ¡Ä‡enje na svim obiÄnim stranicama i zaobilazi provjeru ÄiÅ¡Ä‡anja za anonimne korisnike omoguÄ‡ujuÄ‡i brzo praÅ¾njenje keÅ¡a',
+);
+
+/** Catalan (catalÃ )
+ * @author SMP
+ */
+$messages['ca'] = array(
+	'purge' => 'purga',
+);
+
+/** Czech (Äesky)
+ * @author Mormegil
+ */
+$messages['cs'] = array(
+	'purge' => 'obnovit',
+	'purge-desc' => 'PÅ™idÃ¡vÃ¡ na vÅ¡echny bÄ›Å¾nÃ© strÃ¡nky zÃ¡loÅ¾ku pro obnovenÃ­, ÄÃ­mÅ¾ umoÅ¾Åˆuje rychlÃ© obnovenÃ­ cache',
+);
+
+/** Danish (dansk)
+ * @author Masz
+ */
+$messages['da'] = array(
+	'purge' => 'nulstil cachen',
+	'purge-desc' => "TilfÃ¸jer et 'tÃ¸m cachen-faneblad' til alle normale sider, og gÃ¸r det muligt for anonyme brugere, ikke at skulle bekrÃ¦fte tÃ¸mningen.",
+);
+
+/** German (Deutsch)
+ * @author Kghbln
+ * @author Raimond Spekking
+ */
+$messages['de'] = array(
+	'purge' => 'Cache leeren',
+	'purge-desc' => 'ErgÃ¤nzt den Reiter â€žPurgeâ€œ, der die schnelle LÃ¶schung des Seitencaches ermÃ¶glicht',
+);
+
+/** Lower Sorbian (dolnoserbski)
+ * @author Michawiki
+ */
+$messages['dsb'] = array(
+	'purge' => 'wuprozniÅ›',
+	'purge-desc' => 'PÅ›idawa rejtark "wuprozniÅ›" na wÅ¡ych normalnych bokach, kÃ³taryÅ¾ zmÃ³Å¾nja malsne proznjenje cacha',
+);
+
+/** Greek (Î•Î»Î»Î·Î½Î¹ÎºÎ¬)
+ * @author Omnipaedista
+ * @author ZaDiak
+ */
+$messages['el'] = array(
+	'purge' => 'ÎµÎºÎºÎ±Î¸Î¬ÏÎ¹ÏƒÎ·',
+	'purge-desc' => 'Î ÏÎ¿ÏƒÎ¸Î­Ï„ÎµÎ¹ Î¼Î¯Î± ÎºÎ±ÏÏ„Î­Î»Î± ÎµÎºÎºÎ±Î¸Î¬ÏÎ¹ÏƒÎ·Ï‚ ÏƒÎµ ÏŒÎ»ÎµÏ‚ Ï„Î¹Ï‚ ÎºÎ±Î½Î¿Î½Î¹ÎºÎ­Ï‚ ÏƒÎµÎ»Î¯Î´ÎµÏ‚ ÎºÎ±Î¹ Ï€Î±ÏÎ±ÎºÎ¬Î¼Ï€Ï„ÎµÎ¹ Ï„Î¿Î½ Î­Î»ÎµÎ³Ï‡Î¿ ÎµÎºÎºÎ±Î¸Î¬ÏÎ¹ÏƒÎ·Ï‚ Î³Î¹Î± Î±Î½ÏŽÎ½Ï…Î¼Î¿Ï…Ï‚ Ï‡ÏÎ®ÏƒÏ„ÎµÏ‚ ÎµÏ€Î¹Ï„ÏÎ­Ï€Î¿Î½Ï„Î±Ï‚ Ï„Î· Î³ÏÎ®Î³Î¿ÏÎ· ÎµÎºÎºÎ±Î¸Î¬ÏÎ¹ÏƒÎ· Ï„Î·Ï‚ Î»Î±Î½Î¸Î¬Î½Î¿Ï…ÏƒÎ±Ï‚ Î¼Î½Î®Î¼Î·Ï‚',
+);
+
+/** Esperanto (Esperanto)
+ * @author Melancholie
+ */
+$messages['eo'] = array(
+	'purge' => 'forviÅigi',
+);
+
+/** Spanish (espaÃ±ol)
+ * @author McDutchie
+ * @author Sanbec
+ */
+$messages['es'] = array(
+	'purge' => 'purgar',
+	'purge-desc' => 'AÃ±ade una pestaÃ±a de purgado en todas las pÃ¡ginas normales permitiendo el purgado rÃ¡pido de la cachÃ©',
+);
+
+/** Estonian (eesti)
+ * @author Avjoska
+ */
+$messages['et'] = array(
+	'purge' => 'TÃ¼hjenda',
+);
+
+/** Finnish (suomi)
+ * @author Crt
+ * @author Str4nd
+ */
+$messages['fi'] = array(
+	'purge' => 'tyhjennÃ¤ vÃ¤limuisti',
+	'purge-desc' => 'LisÃ¤Ã¤ vÃ¤limuistin tyhjennysvÃ¤lilehden kaikille tavallisille sivuille mahdollistaen vÃ¤limuistin nopean tyhjennyksen.',
+);
+
+/** French (franÃ§ais)
+ * @author Gomoko
+ * @author Grondin
+ */
+$messages['fr'] = array(
+	'purge' => 'purger',
+	'purge-desc' => 'Ajoute un onglet Â«PurgerÂ» sur toutes les pages normales, permettant une purge rapide du cache',
+);
+
+/** Franco-ProvenÃ§al (arpetan)
+ * @author ChrisPtDe
+ */
+$messages['frp'] = array(
+	'purge' => 'purgiÃ©r',
+);
+
+/** Galician (galego)
+ * @author ToliÃ±o
+ * @author XosÃ©
+ */
+$messages['gl'] = array(
+	'purge' => 'Limpar',
+	'purge-desc' => 'Engade a lapela "Limpar" en todas as pÃ¡xinas normais, permitindo unha purga rÃ¡pida da cachÃ©',
+);
+
+/** Swiss German (Alemannisch)
+ * @author Als-ChlÃ¤mens
+ * @author Als-Holder
+ */
+$messages['gsw'] = array(
+	'purge' => 'Cache lÃ¤Ã¤re',
+	'purge-desc' => 'ErgÃ¤nzt de Ryter â€žPurgeâ€œ, wo e schnÃ¤lli Cache-Leschig mÃ¶gli macht.',
+);
+
+/** Gujarati (àª—à«àªœàª°àª¾àª¤à«€)
+ * @author Ashok modhvadia
+ */
+$messages['gu'] = array(
+	'purge' => 'àªµàª¿àª°à«‡àªšàª¨',
+	'purge-desc' => 'àª¤àª®àª¾àª® àª¸àª¾àª®àª¾àª¨à«àª¯ àªªàª¾àª¨àª¾àª“ àªªàª° â€™àªµàª¿àª°à«‡àªšàª¨â€™ àªŸà«‡àª¬ àª‰àª®à«‡àª°à«‹, àª…àª¨à«‡ àª¨àª¨àª¾àª®àª¾ àª¯à«‹àª—àª¦àª¾àª¨àª•àª°à«àª¤àª¾ àª®àª¾àªŸà«‡ àª¸àª‚àª—à«àª°àª¹àª¨àª¾àª‚ àªàª¡àªªà«€ àªµàª¿àª°à«‡àªšàª¨ àª®àª¾àªŸà«‡ àªµàª¿àª°à«‡àªšàª¨ àªšàª•àª¾àª¸àª£à«€ àªŸàª¾àª³à«‹',
+);
+
+/** Hebrew (×¢×‘×¨×™×ª)
+ * @author Amire80
+ * @author Rotemliss
+ * @author YaronSh
+ */
+$messages['he'] = array(
+	'purge' => '×¨×¢× ×•×Ÿ ×”×ž×˜×ž×•×Ÿ',
+	'purge-desc' => '×”×•×¡×¤×ª ×œ×©×•× ×™×ª ×¨×™×§×•×Ÿ ×œ×›×œ ×”×“×¤×™× ×”×¨×’×™×œ×™× ×©×ž××¤×©×¨×ª ×¨×™×§×•×Ÿ ×–×¨×™×– ×©×œ ×”×ž×˜×ž×•×Ÿ',
+);
+
+/** Hindi (à¤¹à¤¿à¤¨à¥à¤¦à¥€)
+ * @author Kaustubh
+ */
+$messages['hi'] = array(
+	'purge' => 'à¤¸à¤¾à¤« à¤•à¤°à¥‡à¤‚ (purge)',
+);
+
+/** Croatian (hrvatski)
+ * @author Ex13
+ * @author SpeedyGonsales
+ */
+$messages['hr'] = array(
+	'purge' => 'oÄisti cache',
+	'purge-desc' => 'Dodaje oÄisti cache karticu na svim obiÄnim stranicama, te zaobilazi provjeru cache-a za anonimne suradnike omoguÄ‡avajuÄ‡i brzo brisanje memoriranih stranicu u pregledniku',
+);
+
+/** Upper Sorbian (hornjoserbsce)
+ * @author Michawiki
+ */
+$messages['hsb'] = array(
+	'purge' => 'wuprÃ³zdniÄ‡',
+	'purge-desc' => 'PÅ™idawa rajtark "wuprÃ³zdniÄ‡" za spÄ›Å¡ne wurjedÅºenje pufrowaka na wÅ¡Ä›ch normalnych stronach',
+);
+
+/** Hungarian (magyar)
+ * @author Dani
+ * @author Glanthor Reviol
+ */
+$messages['hu'] = array(
+	'purge' => 'frissÃ­t',
+	'purge-desc' => 'Ãšj fÃ¼let ad hozzÃ¡ minden normÃ¡l laphoz a gyorsÃ­tÃ³tÃ¡r kiÃ¼rÃ­tÃ©sÃ©re, Ã©s kihagyja az Ã¼rÃ­tettsÃ©g-ellenÅ‘rzÃ©st nem bejelentkezett felhasznÃ¡lÃ³k esetÃ©n, ami lehetÅ‘vÃ© teszi a gyorsÃ­tÃ³tÃ¡r gyors Ã¼rÃ­tÃ©sÃ©t',
+);
+
+/** Interlingua (interlingua)
+ * @author McDutchie
+ */
+$messages['ia'] = array(
+	'purge' => 'purgar',
+	'purge-desc' => 'Adde un scheda "purgar" in tote le paginas normal, permittente un purga rapide del cache',
+);
+
+/** Indonesian (Bahasa Indonesia)
+ * @author Bennylin
+ * @author Irwangatot
+ * @author IvanLanin
+ */
+$messages['id'] = array(
+	'purge' => 'Bersihkan',
+	'purge-desc' => 'Menambahkan tab pembersihan pada semua halaman normal dan meloloskan halangan bagi pengguna anonim untuk melakukan pembersihan cepat singgahan',
+);
+
+/** Icelandic (Ã­slenska)
+ * @author S.Ã–rvarr.S
+ */
+$messages['is'] = array(
+	'purge' => 'hreinsa',
+);
+
+/** Italian (italiano)
+ * @author McDutchie
+ * @author Pietrodn
+ */
+$messages['it'] = array(
+	'purge' => 'purga',
+	'purge-desc' => 'Aggiunge un tab "purga" su tutte le pagine normali, permettendo uno svuotamento rapido della cache',
+);
+
+/** Japanese (æ—¥æœ¬èªž)
+ * @author Fryed-peach
+ * @author Kahusi
+ * @author Shirayuki
+ */
+$messages['ja'] = array(
+	'purge' => 'ã‚­ãƒ£ãƒƒã‚·ãƒ¥ã‚’å‰Šé™¤',
+	'purge-desc' => 'ã™ã¹ã¦ã®é€šå¸¸ãƒšãƒ¼ã‚¸ã«ã€è¿…é€Ÿã«ã‚­ãƒ£ãƒƒã‚·ãƒ¥ã‚’å‰Šé™¤ã™ã‚‹ãŸã‚ã®ã€Œã‚­ãƒ£ãƒƒã‚·ãƒ¥ã‚’å‰Šé™¤ã€ã‚¿ãƒ–ã‚’è¿½åŠ ã™ã‚‹',
+);
+
+/** Javanese (Basa Jawa)
+ * @author Meursault2004
+ */
+$messages['jv'] = array(
+	'purge' => 'kosongna',
+);
+
+/** Khmer (áž—áž¶ážŸáž¶ážáŸ’áž˜áŸ‚ážš)
+ * @author Lovekhmer
+ */
+$messages['km'] = array(
+	'purge' => 'áž›áž»áž”áž”áŸ†áž”áž¶ážáŸ‹',
+);
+
+/** Korean (í•œêµ­ì–´)
+ * @author ITurtle
+ * @author Kwj2772
+ */
+$messages['ko'] = array(
+	'purge' => 'ìƒˆë¡œ ê³ ì¹¨',
+	'purge-desc' => 'ëª¨ë“  ì¼ë°˜ ë¬¸ì„œì— ìƒˆë¡œ ê³ ì¹¨ íƒ­ì„ ì¶”ê°€í•©ë‹ˆë‹¤.',
+);
+
+/** Colognian (Ripoarisch)
+ * @author Purodha
+ */
+$messages['ksh'] = array(
+	'purge' => 'Zwescheschpeischer leddisch maache',
+	'purge-desc' => 'MÃ¤Ã¤t ene â€ž{{int:purge}}â€œ Lenk op jede nomaale Sigg, esu dat jeder flÃ¶ck der Zwescheschpeischer fÃ¶r en Sigg leddisch maache kann.',
+);
+
+/** Luxembourgish (LÃ«tzebuergesch)
+ * @author Robby
+ */
+$messages['lb'] = array(
+	'purge' => 'eidel maachen',
+	'purge-desc' => 'Setzt e "Purge" Onglet op all normal SÃ¤iten derbÃ¤i an erlaabt et fir den TÃ«schespÃ¤icher (Cache) sÃ©ier eidelzemaachen',
+);
+
+/** Lithuanian (lietuviÅ³)
+ * @author Pdxx
+ */
+$messages['lt'] = array(
+	'purge' => 'atnaujinti',
+);
+
+/** Macedonian (Ð¼Ð°ÐºÐµÐ´Ð¾Ð½ÑÐºÐ¸)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'purge' => 'Ð¸ÑÑ‡Ð¸ÑÑ‚Ð¸',
+	'purge-desc' => 'Ð”Ð¾Ð´Ð°Ð²Ð° Ñ˜Ð°Ð·Ð¸Ñ‡Ðµ Ð·Ð° Ð¿Ñ€ÐµÑ‡Ð¸ÑÑ‚ÑƒÐ²Ð°ÑšÐµ ÐºÐµÑˆ Ð½Ð° ÑÐ¸Ñ‚Ðµ Ð½Ð¾Ñ€Ð¼Ð°Ð»Ð½Ð¸ ÑÑ‚Ñ€Ð°Ð½Ð¸Ñ†Ð¸, ÑˆÑ‚Ð¾ Ð¾Ð²Ð¾Ð·Ð¼Ð¾Ð¶ÑƒÐ²Ð° Ð±Ñ€Ð·Ð¾ Ñ‡Ð¸ÑÑ‚ÐµÑšÐµ Ð½Ð° ÐºÐµÑˆÐ¾Ñ‚',
+);
+
+/** Marathi (à¤®à¤°à¤¾à¤ à¥€)
+ * @author Kaustubh
+ */
+$messages['mr'] = array(
+	'purge' => 'à¤¸à¥à¤µà¤šà¥à¤› à¤•à¤°à¤¾ (purge)',
+	'purge-desc' => 'à¤¸à¤°à¥à¤µ à¤¸à¤¾à¤§à¤¾à¤°à¤£ à¤ªà¤¾à¤¨à¤¾à¤‚à¤µà¤° à¤à¤• à¤¸à¥à¤µà¤šà¥à¤›à¤¤à¥‡à¤šà¤¾ à¤Ÿà¥…à¤¬ à¤µà¤¾à¤¢à¤µà¤¿à¤¤à¥‹, à¤µ à¤…à¤¨à¤¾à¤®à¤¿à¤• à¤¸à¤¦à¤¸à¥à¤¯à¤¾à¤‚à¤¸à¤¾à¤ à¥€ à¤ªà¤°à¥à¤œ à¤¤à¤ªà¤¾à¤¸à¤£à¥€ à¤µà¤—à¤³à¥‚à¤¨ à¤²à¤µà¤•à¤° à¤¸à¤¯ à¤¸à¥à¤µà¤šà¥à¤› à¤•à¤°à¤£à¥à¤¯à¤¾à¤šà¥€ à¤¸à¥à¤µà¤¿à¤§à¤¾ à¤¦à¥‡à¤¤à¥‹',
+);
+
+/** Malay (Bahasa Melayu)
+ * @author Anakmalaysia
+ */
+$messages['ms'] = array(
+	'purge' => 'singkirkan',
+	'purge-desc' => 'Membubuh tab singkirkan pada semua laman biasa untuk mempercepat penyingkiran cache',
+);
+
+/** Erzya (ÑÑ€Ð·ÑÐ½ÑŒ)
+ * @author Botuzhaleny-sodamo
+ */
+$messages['myv'] = array(
+	'purge' => 'Ð¿Ð°Ð½ÐµÐ¼Ñ',
+);
+
+/** Nahuatl (NÄhuatl)
+ * @author Fluence
+ */
+$messages['nah'] = array(
+	'purge' => 'ticchipÄhuÄz',
+);
+
+/** Norwegian BokmÃ¥l (norsk (bokmÃ¥l)â€Ž) */
+$messages['nb'] = array(
+	'purge' => 'gjenoppfrisk',
+	'purge-desc' => 'Legger til en fane for gjenoppfrisking pÃ¥ alle vanlige sider, og slÃ¥r av gjenoppfriskingssjekken for anonyme brukere, hvilket gjÃ¸r at mellomlageret kan oppdateres raskere',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'purge' => 'cache legen',
+	'purge-desc' => 'Voegt een tabblad "cache legen" toe op alle normale pagina\'s zodat de cache snel gepurged kan worden',
+);
+
+/** Norwegian Nynorsk (norsk (nynorsk)â€Ž)
+ * @author Harald Khan
+ * @author Njardarlogar
+ */
+$messages['nn'] = array(
+	'purge' => 'oppfrisk',
+	'purge-desc' => 'Legg til ei fana for oppfrisking pÃ¥ alle vanlege sider, og slÃ¥r av oppfriskingssjekken for anonyme brukarar, noko som gjer at mellomlageret kan verta oppdartert raskare',
+);
+
+/** Occitan (occitan)
+ * @author Cedric31
+ */
+$messages['oc'] = array(
+	'purge' => 'purgar',
+	'purge-desc' => "Apond un onglet Â« Purgar Â» sus totas las paginas normalas e activa sa seleccion pels utilizaires anonims per una purga rapida de l'amagatal.",
+);
+
+/** Polish (polski)
+ * @author BeginaFelicysym
+ * @author Sp5uhe
+ */
+$messages['pl'] = array(
+	'purge' => 'wyczyÅ›Ä‡',
+	'purge-desc' => "Dodaje zakÅ‚adkÄ™ '''wyczyÅ›Ä‡''' na wszystkich zwykÅ‚ych stronach aby szybko wyczyÅ›ciÄ‡ pamiÄ™Ä‡ podrÄ™cznÄ…",
+);
+
+/** Piedmontese (PiemontÃ¨is)
+ * @author BorichÃ¨t
+ * @author DragonÃ²t
+ */
+$messages['pms'] = array(
+	'purge' => 'polida',
+	'purge-desc' => 'A gionta un feuj Ã«d purga a tute le pÃ gine normaj an pÃ«rmÃ«ttend na polidada lesta dla memÃ²ria local',
+);
+
+/** Portuguese (portuguÃªs)
+ * @author 555
+ * @author Hamilton Abreu
+ * @author Malafaya
+ * @author McDutchie
+ */
+$messages['pt'] = array(
+	'purge' => 'purgar',
+	'purge-desc' => "Adiciona um separador \"purgar\" a todas as pÃ¡ginas normais, permitindo uma purga rÃ¡pida da ''cache''",
+);
+
+/** Brazilian Portuguese (portuguÃªs do Brasil)
+ * @author Eduardo.mps
+ * @author McDutchie
+ */
+$messages['pt-br'] = array(
+	'purge' => 'purgar',
+	'purge-desc' => 'Adiciona uma aba para purgar em todas as pÃ¡ginas normais, permitindo um purga rÃ¡pida da cache',
+);
+
+/** Romanian (romÃ¢nÄƒ)
+ * @author Stelistcristi
+ */
+$messages['ro'] = array(
+	'purge' => 'curÄƒÅ£Äƒ',
+);
+
+/** tarandÃ­ne (tarandÃ­ne)
+ * @author Joetaras
+ */
+$messages['roa-tara'] = array(
+	'purge' => 'sdevache',
+	'purge-desc' => "Aggiunge 'na tabelle de pulizzie sus a tutte le pÃ ggene normale accussÃ¬ se pÃ² sdevacÃ  cchiÃ¹ velocemende 'a cache",
+);
+
+/** Russian (Ñ€ÑƒÑÑÐºÐ¸Ð¹)
+ * @author Ferrer
+ * @author ÐÐ»ÐµÐºÑÐ°Ð½Ð´Ñ€ Ð¡Ð¸Ð³Ð°Ñ‡Ñ‘Ð²
+ */
+$messages['ru'] = array(
+	'purge' => 'Ð¾Ñ‡Ð¸ÑÑ‚Ð¸Ñ‚ÑŒ',
+	'purge-desc' => 'Ð”Ð¾Ð±Ð°Ð²Ð»ÑÐµÑ‚ Ð·Ð°ÐºÐ»Ð°Ð´ÐºÑƒ Ð¾Ñ‡Ð¸ÑÑ‚ÐºÐ¸ Ð½Ð° Ð²ÑÐµ Ð¾Ð±Ñ‹Ñ‡Ð½Ñ‹Ðµ ÑÑ‚Ñ€Ð°Ð½Ð¸Ñ†Ñ‹, Ð´Ð»Ñ ÐºÐ¾Ñ‚Ð¾Ñ€Ñ‹Ñ… Ð²Ð¾Ð·Ð¼Ð¾Ð¶Ð½Ð° Ð±Ñ‹ÑÑ‚Ñ€Ð°Ñ Ð¾Ñ‡Ð¸ÑÑ‚ÐºÐ° ÐºÐµÑˆÐ°',
+);
+
+/** Slovak (slovenÄina)
+ * @author Helix84
+ * @author Teslaton
+ */
+$messages['sk'] = array(
+	'purge' => 'preÄistiÅ¥',
+	'purge-desc' => 'PridÃ¡va zÃ¡loÅ¾ku â€žPreÄistiÅ¥â€œ na vÅ¡etky beÅ¾nÃ© strÃ¡nky, ÄÃ­m umoÅ¾Åˆuje anonymnÃ½m pouÅ¾Ã­vateÄ¾om rÃ½chle preÄistenie vyrovnÃ¡vacej pamÃ¤te',
+);
+
+/** Seeltersk (Seeltersk)
+ * @author Pyt
+ */
+$messages['stq'] = array(
+	'purge' => 'Cache loosmoakje',
+	'purge-desc' => 'FÃ¶iget n â€žPurgeâ€œ-Rieder tou foar aal normoale Sieden tou ju gaue Cache-LÃ¶Ã¶senge un uumgungt ju Purge-WrÃ¶ige foar anonyme Benutsere',
+);
+
+/** Swedish (svenska)
+ * @author M.M.S.
+ * @author WikiPhoenix
+ */
+$messages['sv'] = array(
+	'purge' => 'rensa',
+	'purge-desc' => 'LÃ¤gger till en flik fÃ¶r rensning pÃ¥ alla vanliga sidor, vilket gÃ¶r att cachen kan rensas snabbare',
+);
+
+/** Thai (à¹„à¸—à¸¢)
+ * @author Harley Hartwell
+ * @author Passawuth
+ */
+$messages['th'] = array(
+	'purge' => 'à¸¥à¹‰à¸²à¸‡à¹à¸„à¸Š',
+	'purge-desc' => 'à¹€à¸žà¸´à¹ˆà¸¡à¹à¸–à¸š "à¸¥à¹‰à¸²à¸‡à¹à¸„à¸Š" à¹ƒà¸™à¸«à¸™à¹‰à¸²à¸—à¸±à¹ˆà¸§à¹„à¸›à¸—à¸±à¹‰à¸‡à¸«à¸¡à¸” à¸£à¸§à¸¡à¸–à¸¶à¸‡à¸œà¸¹à¹‰à¹ƒà¸Šà¹‰à¸™à¸´à¸£à¸™à¸²à¸¡à¸ªà¸²à¸¡à¸²à¸£à¸–à¸œà¹ˆà¸²à¸™à¸à¸²à¸£à¸¢à¸·à¸™à¸¢à¸±à¸™à¸à¸²à¸£à¸¥à¹‰à¸²à¸‡à¹à¸„à¸Š à¹€à¸žà¸·à¹ˆà¸­à¸„à¸§à¸²à¸¡à¸£à¸§à¸”à¹€à¸£à¹‡à¸§à¹ƒà¸™à¸à¸²à¸£à¸¥à¹‰à¸²à¸‡à¹à¸„à¸Šà¹„à¸”à¹‰',
+);
+
+/** Tagalog (Tagalog)
+ * @author AnakngAraw
+ */
+$messages['tl'] = array(
+	'purge' => 'purgahin/linisin',
+	'purge-desc' => 'Nagdaragdag ng isang laylayan ng pampurga sa ibabaw ng lahat ng pangkaraniwang mga pahina para sa maliksing pagpupurga ng taguan',
+);
+
+/** Tongan (lea faka-Tonga)
+ * @author TauÊ»olunga
+ */
+$messages['to'] = array(
+	'purge' => 'fakamaÊ»a',
+);
+
+/** Turkish (TÃ¼rkÃ§e)
+ * @author Karduelis
+ * @author Vito Genovese
+ */
+$messages['tr'] = array(
+	'purge' => 'Temizle',
+	'purge-desc' => 'TÃ¼m normal sayfalara bir temizleme sekmesi ekler ve anonim kullanÄ±cÄ±lar iÃ§in temizleme kontrolÃ¼nÃ¼ geÃ§erek Ã¶n belleÄŸin hÄ±zlÄ± temizlenmesini saÄŸlar',
+);
+
+/** Ukrainian (ÑƒÐºÑ€Ð°Ñ—Ð½ÑÑŒÐºÐ°)
+ * @author Prima klasy4na
+ * @author Ð¢ÐµÑÑ‚
+ */
+$messages['uk'] = array(
+	'purge' => 'Ð¾Ñ‡Ð¸ÑÑ‚Ð¸Ñ‚Ð¸',
+	'purge-desc' => 'Ð”Ð¾Ð´Ð°Ñ” Ð²ÐºÐ»Ð°Ð´ÐºÑƒ Ð¾Ñ‡Ð¸ÑÑ‚ÐºÐ¸ Ð½Ð° Ð²ÑÑ– Ð·Ð²Ð¸Ñ‡Ð°Ð¹Ð½Ñ– ÑÑ‚Ð¾Ñ€Ñ–Ð½ÐºÐ¸, Ñ‰Ð¾ Ð´Ð¾Ð·Ð²Ð¾Ð»ÑÑ” ÑˆÐ²Ð¸Ð´ÐºÐ¾ Ð¾Ñ‡Ð¸Ñ‰Ð°Ñ‚Ð¸ ÐºÐµÑˆ',
+);
+
+/** Urdu (Ø§Ø±Ø¯Ùˆ)
+ * @author à¶´à·ƒà·’à¶³à·” à¶šà·à·€à·’à¶±à·Šà¶¯
+ */
+$messages['ur'] = array(
+	'purge' => 'Ù¾Ø§Ú©',
+);
+
+/** Vietnamese (Tiáº¿ng Viá»‡t)
+ * @author Minh Nguyen
+ * @author Vinhtantran
+ */
+$messages['vi'] = array(
+	'purge' => 'lÃ m tÆ°Æ¡i',
+	'purge-desc' => 'ThÃªm má»™t tab táº©y bá»™ Ä‘á»‡m táº¡i nhá»¯ng trang bÃ¬nh thÆ°á»ng Ä‘á»ƒ cho táº©y nhanh bá»™ Ä‘á»‡m',
+);
+
+/** Simplified Chinese (ä¸­æ–‡ï¼ˆç®€ä½“ï¼‰â€Ž)
+ * @author Gzdavidwong
+ */
+$messages['zh-hans'] = array(
+	'purge' => 'æ¸…é™¤',
+	'purge-desc' => 'æ–°å¢žä¸€ä¸ªæ¸…ç©ºå¿«å–çš„æŒ‰é’®åˆ°æ‰€æœ‰é¡µé¢',
+);
+
+/** Traditional Chinese (ä¸­æ–‡ï¼ˆç¹é«”ï¼‰â€Ž)
+ * @author Horacewai2
+ * @author Waihorace
+ * @author Wrightbus
+ */
+$messages['zh-hant'] = array(
+	'purge' => 'æ¸…é™¤',
+	'purge-desc' => 'æ–°å¢žä¸€å€‹æ¸…ç©ºå¿«å–çš„æŒ‰éˆ•åˆ°æ‰€æœ‰é é¢',
+);
+
